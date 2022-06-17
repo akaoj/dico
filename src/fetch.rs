62d0fdefa60c7619cb9cf322a472ad7d597daeb9
Valentin Bremond <v.bremond@criteo.com>
2022-06-17 15:46:23 +0200

    WIP: start working on REST API

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,18 +1,15 @@
-use std::io;
-
 use std::fs;
+use std::io;
 use std::panic;
 use std::path::PathBuf;
 use std::thread;
 
 extern crate flume;
-extern crate parse_wiktionary_en;
 extern crate reqwest;
 
 use dico::errors::DicoError;
 use dico::Word;
 
-pub fn fetch(path: &PathBuf, language: &str) -> Result<usize, DicoError> {
-
+pub fn fetch(stdin: io::Stdin, path: &PathBuf, language: &str) -> Result<usize, DicoError> {
 	Ok(1)
 }
